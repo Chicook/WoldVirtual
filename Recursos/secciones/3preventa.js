6cf86fe585b0a01235947d2ab917aaee6003d022
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2023-03-31 21:11:25 +0200

    Update 3preventa.js

--- Recursos/secciones/3preventa.js
+++ Recursos/secciones/3preventa.js
@@ -1,3 +1,30 @@
+const connectMetamaskButton = document.querySelector('#connect-metamask');
+
+// Función que se ejecuta al hacer clic en el botón de Metamask
+function connectMetamask() {
+  // Verificamos si Metamask está instalado en el navegador
+  if (typeof window.ethereum !== 'undefined') {
+    console.log('Metamask está instalado');
+
+    // Conectamos Metamask
+    window.ethereum.request({ method: 'eth_requestAccounts' })
+      .then(() => {
+        console.log('Metamask conectado');
+        // Aquí puedes agregar más código para realizar operaciones con Metamask
+      })
+      .catch((error) => {
+        console.error('Error al conectar Metamask', error);
+      });
+  } else {
+    console.error('Metamask no está instalado');
+  }
+}
+
+// Agregamos un evento de clic al botón de Metamask
+connectMetamaskButton.addEventListener('click', connectMetamask);
+
+
+
 // Crea un array con los precios de cada fase
 
 const precios = [0.0005, 0.00051, 0.00052, 0.00053, 0.00054, 0.00055, 0.00056, 0.00057, 0.00058, 0.0006];
