7a272156e48107fe982f6bb3720eed39dab5c16a
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2023-03-31 13:49:47 +0200

    Update pb.js

--- puentedeblokchain/pb.js
+++ puentedeblokchain/pb.js
@@ -1,70 +1,32 @@
-// Conexión a la red de Ethereum
-const web3 = new Web3(Web3.givenProvider);
+const form = document.querySelector('#exchange-form');
+const alert = document.querySelector('.alert');
 
-// Dirección del contrato en la red de Ethereum
-const contractAddress = "0x123456789ABCDEF...";
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
 
-// Instancia del contrato en la red de Ethereum
-const contractInstance = new web3.eth.Contract(abi, contractAddress);
+  const fromNetwork = form.elements['from-network'].value;
+  const toNetwork = form.elements['to-network'].value;
+  const fromToken = form.elements['from-token'].value;
+  const toToken = form.elements['to-token'].value;
+  const amount = form.elements['amount'].value;
+  const wallet = form.elements['wallet'].value;
 
-// Obtener el balance de tokens de la cuenta del usuario
-async function getBalance(tokenAddress, account) {
-  const tokenInstance = new web3.eth.Contract(erc20Abi, tokenAddress);
-  const balance = await tokenInstance.methods.balanceOf(account).call();
-  return balance;
-}
+  alert.style.display = 'none';
 
-// Transferir tokens entre redes de blockchain
-async function transferTokens(fromToken, toToken, amount) {
-  const accounts = await web3.eth.getAccounts();
-  const fromTokenInstance = new web3.eth.Contract(erc20Abi, fromToken);
-  const toTokenInstance = new web3.eth.Contract(erc20Abi, toToken);
-  const fee = Math.round(amount * 0.001);
-  const totalAmount = amount + fee;
-  const allowance = await fromTokenInstance.methods.allowance(accounts[0], contractAddress).call();
-  if (allowance < totalAmount) {
-    await fromTokenInstance.methods.approve(contractAddress, totalAmount).send({ from: accounts[0] });
+  try {
+    const txHash = await exchange(fromNetwork, toNetwork, fromToken, toToken, amount, wallet);
+
+    alert.classList.add('alert-success');
+    alert.textContent = `Exchange successful! Tx hash: ${txHash}`;
+  } catch (error) {
+    alert.classList.add('alert-danger');
+    alert.textContent = error.message;
   }
-  await contractInstance.methods.transferTokens(fromToken, toToken, amount, fee).send({ from: accounts[0] });
-}
 
-// Mostrar el balance de tokens de la cuenta del usuario
-async function showBalance(tokenAddress, account) {
-  const balance = await getBalance(tokenAddress, account);
-  const balanceElement = document.getElementById("balance-" + tokenAddress);
-  balanceElement.innerText = "Balance: " + balance;
-}
+  alert.style.display = 'block';
+});
 
-// Mostrar los tokens disponibles para transferir
-async function showTokens() {
-  const accounts = await web3.eth.getAccounts();
-  const fromTokenElement = document.getElementById("from-token");
-  const toTokenElement = document.getElementById("to-token");
-  const balanceElements = document.querySelectorAll(".balance");
-  const tokens = [
-    { name: "Ethereum", address: "0x123456789ABCDEF...", symbol: "ETH" },
-    { name: "Binance Coin", address: "0x123456789ABCDEF...", symbol: "BNB" },
-    { name: "Bitcoin", address: "0x123456789ABCDEF...", symbol: "BTC" },
-    { name: "WoldcoinVirtual", address: "0x123456789ABCDEF...", symbol: "WLCV" },
-    // ...
-  ];
-  for (let i = 0; i < tokens.length; i++) {
-    const balance = await getBalance(tokens[i].address, accounts[0]);
-    const option = document.createElement("option");
-    option.text = tokens[i].name + " (" + tokens[i].symbol + ")";
-    option.value = tokens[i].address;
-    fromTokenElement.add(option);
-    toTokenElement.add(option.cloneNode(true));
-    const balanceElement = document.createElement("div");
-    balanceElement.className = "balance";
-    balanceElement.id = "balance-" + tokens[i].address;
-    balanceElement.innerText = "Balance: " + balance;
-    balanceElements[i].appendChild(balanceElement);
-  }
+async function exchange(fromNetwork, toNetwork, fromToken, toToken, amount, wallet) {
+  // Implementar lógica de intercambio de tokens
+  // ...
 }
-
-// Actualizar los balances de tokens de la cuenta del usuario
-async function updateBalances() {
-  const accounts = await web3.eth.getAccounts();
-  const tokens = [
-    { name: "Ethereum",
