875f8fbc6d3f11e9d722f9ba499b0ba582e2408c
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2023-03-31 13:47:01 +0200

    Update Puentedeblokchain.sol

--- puentedeblokchain/Puentedeblokchain.sol
+++ puentedeblokchain/Puentedeblokchain.sol
@@ -1,75 +1,70 @@
 pragma solidity ^0.8.0;
 
-interface ERC20 {
+contract CryptoTrading {
+    address owner;
+    uint256 fee = 10; // fee en base 10000 (0.1%)
+    mapping(address => mapping(string => uint256)) balances; // balances[tokenAddress][userAddress]
+    mapping(string => address) tokenAddresses;
+    
+    constructor() {
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the contract owner can perform this action");
+        _;
+    }
+    
+    function setFee(uint256 newFee) public onlyOwner {
+        require(newFee <= 1000, "Fee cannot exceed 10%");
+        fee = newFee;
+    }
+    
+    function addToken(string memory symbol, address tokenAddress) public onlyOwner {
+        tokenAddresses[symbol] = tokenAddress;
+    }
+    
+    function getBalance(string memory symbol, address user) public view returns (uint256) {
+        address tokenAddress = tokenAddresses[symbol];
+        require(tokenAddress != address(0), "Token not supported");
+        return balances[user][symbol];
+    }
+    
+    function deposit(string memory symbol, uint256 amount) public {
+        address tokenAddress = tokenAddresses[symbol];
+        require(tokenAddress != address(0), "Token not supported");
+        require(amount > 0, "Amount must be greater than 0");
+        require(IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount), "Token transfer failed");
+        balances[msg.sender][symbol] += amount;
+    }
+    
+    function withdraw(string memory symbol, uint256 amount) public {
+        address tokenAddress = tokenAddresses[symbol];
+        require(tokenAddress != address(0), "Token not supported");
+        require(amount > 0, "Amount must be greater than 0");
+        require(balances[msg.sender][symbol] >= amount, "Insufficient balance");
+        balances[msg.sender][symbol] -= amount;
+        require(IERC20(tokenAddress).transfer(msg.sender, amount), "Token transfer failed");
+    }
+    
+    function trade(string memory symbolSell, uint256 amountSell, string memory symbolBuy, uint256 amountBuy) public {
+        address tokenAddressSell = tokenAddresses[symbolSell];
+        address tokenAddressBuy = tokenAddresses[symbolBuy];
+        require(tokenAddressSell != address(0) && tokenAddressBuy != address(0), "Tokens not supported");
+        require(amountSell > 0 && amountBuy > 0, "Amounts must be greater than 0");
+        require(balances[msg.sender][symbolSell] >= amountSell, "Insufficient balance");
+        require(IERC20(tokenAddressSell).allowance(msg.sender, address(this)) >= amountSell, "Token allowance too low");
+        uint256 feeAmount = amountSell * fee / 10000;
+        uint256 amountSellMinusFee = amountSell - feeAmount;
+        require(IERC20(tokenAddressSell).transferFrom(msg.sender, address(this), amountSell), "Token transfer failed");
+        require(IERC20(tokenAddressBuy).transfer(msg.sender, amountBuy), "Token transfer failed");
+        balances[msg.sender][symbolSell] -= amountSell;
+        balances[msg.sender][symbolBuy] += amountBuy;
+        balances[owner][symbolSell] += feeAmount;
+    }
+}
+
+interface IERC20 {
     function transfer(address to, uint256 value) external returns (bool);
-    function balanceOf(address who) external view returns (uint256);
-    function allowance(address owner, address spender) external view returns (uint256);
     function transferFrom(address from, address to, uint256 value) external returns (bool);
-    function approve(address spender, uint256 value) external returns (bool);
-}
-
-contract TokenBridge {
-    mapping (address => uint256) public balances;
-    mapping (address => mapping (address => uint256)) public allowed;
-
-    address public token;
-    uint256 public rate;
-    uint256 public commission = 10;
-    address public commissionWallet;
-    
-    mapping (uint256 => address) public supportedTokens;
-    uint256 public supportedTokensCount;
-    
-    constructor(address _token, uint256 _rate, address _commissionWallet) {
-        token = _token;
-        rate = _rate;
-        commissionWallet = _commissionWallet;
-        
-        // Agregar la red principal de Bitcoin
-        supportedTokensCount = 1;
-        supportedTokens[0] = 0x0000000000000000000000000000000000000000; // Dirección de Bitcoin
-    }
-    
-    function transferToken(address _token, address _to, uint256 _value) private {
-        ERC20(_token).transfer(_to, _value);
-    }
-    
-    function transferFromToken(address _token, address _from, address _to, uint256 _value) private {
-        ERC20(_token).transferFrom(_from, _to, _value);
-    }
-    
-    function setRate(uint256 _rate) public {
-        rate = _rate;
-    }
-    
-    function setCommission(uint256 _commission) public {
-        commission = _commission;
-    }
-    
-    function setCommissionWallet(address _commissionWallet) public {
-        commissionWallet = _commissionWallet;
-    }
-    
-    function addSupportedToken(address _token) public {
-        supportedTokens[supportedTokensCount] = _token;
-        supportedTokensCount++;
-    }
-    
-    function convertTokens(uint256 _amount, uint256 _tokenIndex) public {
-        require(supportedTokens[_tokenIndex] != address(0), "Token no soportado");
-        
-        // Transferir tokens al contrato
-        transferFromToken(token, msg.sender, address(this), _amount);
-        
-        // Convertir tokens
-        uint256 convertedAmount = _amount * rate;
-        uint256 commissionAmount = (convertedAmount * commission) / 1000;
-        uint256 transferAmount = convertedAmount - commissionAmount;
-        
-        // Transferir tokens convertidos
-        transferToken(supportedTokens[_tokenIndex], msg.sender, transferAmount);
-        
-        // Transferir comisión
-        transferToken(supportedTokens[_tokenIndex], commissionWallet, commissionAmount);
-    }
-}
+    function allowance(address owner, address spender) external view returns (uint256);
