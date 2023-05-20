d3c09263bb367e63c2fc8eae546440daa7d2f2e3
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2023-05-20 20:23:07 +0200

    BEP20 5

--- recursos/contratossol/pendientedevalidacion/BEP20woldcoinvirtual.sol
+++ recursos/contratossol/pendientedevalidacion/BEP20woldcoinvirtual.sol
@@ -2,31 +2,29 @@
 pragma solidity ^0.8.20;
 
 interface IBEP20 {
-        function totalSupply() external view returns (uint256);
-            function balanceOf(address account) external view returns (uint256);
-                function transfer(address recipient, uint256 amount) external returns (bool);
+  
+  function totalSupply() external view returns (uint256);
+ function balanceOf(address account) external view returns (uint256);
+     function transfer(address recipient, uint256 amount) external returns (bool);
 }
 
 contract WoldcoinVirtual is IBEP20 {
-        string public name = "WoldcoinVirtual";
-            string public symbol = "WCV";
-                uint8 public decimals = 3;
-                    uint256 public totalSupply = 30000000 * (3**uint256(decimals));
-                        mapping(address => uint256) public balanceOf;
+string public name = "WoldcoinVirtual";
+ string public symbol = "WCV";
+ uint8 public decimals = 3;
+uint256 public totalSupply = 30000000 * (3**uint256(decimals));
+mapping(address => uint256) public balanceOf;
 
-                            constructor() {
-                                        balanceOf[msg.sender] = totalSupply;
+constructor() {
+balanceOf[msg.sender] = totalSupply;
                             }
 
-                                function transfer(address recipient, uint256 amount) external override returns (bool) {
-                                            require(balanceOf[msg.sender] >= amount, "Insufficient balance");
-                                                    balanceOf[msg.sender] -= amount;
-                                                            balanceOf[recipient] += amount;
-                                                                    return true;
+ function transfer(address recipient, uint256 amount) external override returns (bool) {
+require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+  balanceOf[msg.sender] -= amount;
+ balanceOf[recipient] += amount;
+  return true;
                                 }
 }
 
-                                }
-                            }
-}
-}
+          
