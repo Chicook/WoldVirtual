a842970b463f342f23ead2c0368ceef702f51c59
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2023-05-23 12:57:24 +0200

    Update cv.sol

--- recursos/contratossol/pendientedevalidacion/cv.sol
+++ recursos/contratossol/pendientedevalidacion/cv.sol
@@ -16,42 +16,43 @@ contract WoldcoinVirtual is Ownable {
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
-                                event Transfer(address indexed from, address indexed to, uint256 value);
-                                    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
-                                        constructor() {
-                                                balanceOf[msg.sender] = totalSupply;
-                                                    }
+     constructor() {
+         balanceOf[msg.sender] = totalSupply;
+                                           }
 
-                                                        function transfer(address _to, uint256 _value) public returns (bool) {
-                                                                require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+         function transfer(address _to, uint256 _value) public returns (bool) {
+         require(balanceOf[msg.sender] >= _value, "Insufficient balance");
 
-                                                                        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
-                                                                                balanceOf[_to] = balanceOf[_to].add(_value);
+         balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+         balanceOf[_to] = balanceOf[_to].add(_value);
 
-                                                                                        emit Transfer(msg.sender, _to, _value);
+         emit Transfer(msg.sender, _to, _value);
 
-                                                                                                return true;
-                                                                                                    }
+         return true;
+                    }
 
-                                                                                                        function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
-                                                                                                                require(balanceOf[_from] >= _value, "Insufficient balance");
-                                                                                                                        require(allowance[_from][msg.sender] >= _value, "Insufficient allowance");
+         function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+         require(balanceOf[_from] >= _value, "Insufficient balance");
+         require(allowance[_from][msg.sender] >= _value, "Insufficient allowance");
 
-                                                                                                                                balanceOf[_from] = balanceOf[_from].sub(_value);
-                                                                                                                                        balanceOf[_to] = balanceOf[_to].add(_value);
-                                                                                                                                                allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);
+         balanceOf[_from] = balanceOf[_from].sub(_value);
+         balanceOf[_to] = balanceOf[_to].add(_value);
+         allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);
 
-                                                                                                                                                        emit Transfer(_from, _to, _value);
+         emit Transfer(_from, _to, _value);
 
-                                                                                                                                                                return true;
-                                                                                                                                                                    }
+         return true;
+                    }
 
-                                                                                                                                                                        function approve(address _spender, uint256 _value) public returns (bool) {
-                                                                                                                                                                                allowance[msg.sender][_spender] = _value;
+         function approve(address _spender, uint256 _value) public returns (bool) {
+         allowance[msg.sender][_spender] = _value;
 
-                                                                                                                                                                                        emit Approval(msg.sender, _spender, _value);
+         emit Approval(msg.sender, _spender, _value);
 
-                                                                                                                                                                                                return true;
-                                                                                                                                                                                                    }
-                                                                                                                                                                                                    }
+         return true;
+                    
+                     }
+}
