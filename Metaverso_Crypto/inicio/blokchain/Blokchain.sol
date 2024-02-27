33fc88b6afbc836bef1f62ccbb6504de6f352744
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-02-27 15:22:07 +0100

    Update Blokchain.sol

--- Metaverso_Crypto/inicio/blokchain/Blokchain.sol
+++ Metaverso_Crypto/inicio/blokchain/Blokchain.sol
@@ -236,16 +236,16 @@ contract sueldo {
 }
 
 contract SimpleBlockchain {
-        mapping(address => uint256) public balances;
+mapping(address => uint256) public balances;
 
-            event Transfer(address indexed from, address indexed to, uint256 value);
+event Transfer(address indexed from, address indexed to, uint256 value);
 
-                function transfer(address to, uint256 value) external {
-                        require(balances[msg.sender] >= value, "Saldo insuficiente");
-                                balances[msg.sender] -= value;
-                                        balances[to] += value;
-                                                emit Transfer(msg.sender, to, value);
-                                                    }
+function transfer(address to, uint256 value) external {
+require(balances[msg.sender] >= value, "Saldo insuficiente");
+balances[msg.sender] -= value;
+balances[to] += value;
+emit Transfer(msg.sender, to, value);
+  }
 }
 
 
