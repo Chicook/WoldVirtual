196322a42dbf5e804832d289121c6a54cad0082f
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-02-27 21:39:57 +0100

    Create Wallet.js

---
+++ Metaverso_Crypto/inicio/blokchain/plantilla/Wallet.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  initApp();
+});
+
+function initApp() {
+  console.log('La aplicación se ha inicializado correctamente.');
+  // Aquí continuarías con la lógica de tu aplicación
+}
