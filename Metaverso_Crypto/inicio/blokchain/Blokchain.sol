0d8f9ccb4b2eb71374486d376cad1bbb9ba6bdf0
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-02-27 17:36:40 +0100

    Update Blokchain.sol

--- Metaverso_Crypto/inicio/blokchain/Blokchain.sol
+++ Metaverso_Crypto/inicio/blokchain/Blokchain.sol
@@ -1,6 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+contract TokenICO {
+    address public owner;
+    uint public totalTokens;
+    uint public tokensSold;
+    mapping(address => uint) public balances;
+
+    event TokenPurchased(address buyer, uint amount, uint totalTokensSold);
+
+    modifier onlyOwner() {
+    require(msg.sender == owner, "Only the owner can call this function");
+    _;
+}
+
+    constructor(uint _totalTokens) {
+    owner = msg.sender;
+    totalTokens = _totalTokens;
+}
+
+    function purchaseTokens(uint _amount) external payable {
+    require(tokensSold + _amount <= totalTokens, "Not enough tokens available");
+    require(msg.value == _amount * 1 ether, "Incorrect Ether amount");
+
+    balances[msg.sender] += _amount;
+    tokensSold += _amount;
+
+    emit TokenPurchased(msg.sender, _amount, tokensSold);
+}
+
+    function withdrawFunds() external onlyOwner {
+    payable(owner).transfer(address(this).balance);
+}
+
+}
+
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
