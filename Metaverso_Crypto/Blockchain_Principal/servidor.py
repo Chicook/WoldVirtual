f550a62cdacbb5c7589f477b5976f14a283625b8
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-08-20 15:25:50 +0200

    Update servidor.py

--- Metaverso_Crypto/Blockchain_Principal/servidor.py
+++ Metaverso_Crypto/Blockchain_Principal/servidor.py
@@ -7,11 +7,37 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def index():
+    """
+    Renderiza la plantilla principal.
+    """
     return render_template('index.html')
 
-    @socketio.on('audio_stream')
-    def handle_audio(data):
-        socketio.emit('audio_stream', data)
+@socketio.on('audio_stream')
+def handle_audio(data):
+    """
+    Maneja el evento de transmisión de audio.
+    
+    :param data: Datos de audio transmitidos.
+    """
+    socketio.emit('audio_stream', data)
 
-        if __name__ == '__main__':
-            socketio.run(app, debug=True)
+if __name__ == 'main':
+    socketio.run(app, debug=True)
+
+# import os
+# from flask import Flask, render_template
+# from flask_socketio import SocketIO
+
+# app = Flask(__name__)
+# socketio = SocketIO(app)
+
+# @app.route('/')
+# def index():
+   # return render_template('index.html')
+
+  #  @socketio.on('audio_stream')
+    # def handle_audio(data):
+       # socketio.emit('audio_stream', data)
+
+      #  if __name__ == '__main__':
+           # socketio.run(app, debug=True)
