2befefcce1c92eb268456290a72927bf91fcd1a2
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-08-20 11:25:20 +0000

    add

--- Metaverso_Crypto/blockain principal/usuarios.py
+++ Metaverso_Crypto/blockain principal/usuarios.py
@@ -4,11 +4,11 @@ usuarios = {}
 
 def registrar_usuario(username, password):
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
-        usuarios[username] = hashed_password
+    usuarios[username] = hashed_password
 
 def verificar_credenciales(username, password):
             hashed_password = hashlib.sha256(password.encode()).hexdigest()
-                return usuarios.get(username) == hashed_password
+            return usuarios.get(username) == hashed_password
 
 def manejar_accion(usuario, accion):
     if accion == "explorar":
