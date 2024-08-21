ca14920a5e8c602e37bb69e7d6e39209477fb6ce
Jaldvox <166617485+Jaldvox@users.noreply.github.com>
2024-08-21 21:31:12 +0200

    Update blockchain.py

--- Metaverso_Crypto/inicio/Blockchain_Principal/blockchain.py
+++ Metaverso_Crypto/inicio/Blockchain_Principal/blockchain.py
@@ -1,14 +1,20 @@
-# blockchain 
+# blockchain.py
 
 import hashlib
 import datetime
 
 class Blockchain:
+    """
+    Clase para gestionar una cadena de bloques (blockchain).
+    """
     def __init__(self):
         self.chain = []
         self.crear_bloque_genesis()
 
     def crear_bloque_genesis(self):
+        """
+        Crea el bloque génesis y lo añade a la cadena.
+        """
         genesis_block = {
             'index': 0,
             'timestamp': str(datetime.datetime.now()),
@@ -19,6 +25,9 @@ class Blockchain:
         self.chain.append(genesis_block)
 
     def agregar_bloque(self, data):
+        """
+        Añade un nuevo bloque a la cadena.
+        """
         previous_block = self.chain[-1]
         new_block = {
             'index': len(self.chain),
@@ -30,6 +39,9 @@ class Blockchain:
         self.chain.append(new_block)
 
     def obtener_informacion_cadena(self):
+        """
+        Obtiene información sobre la cadena de bloques.
+        """
         informacion = {
             'longitud': len(self.chain),
             'bloques': [block for block in self.chain],
@@ -37,10 +49,16 @@ class Blockchain:
         return informacion
 
     def hash_block(self, index, timestamp, data, previous_hash):
+        """
+        Genera el hash para un bloque.
+        """
         block_string = f"{index}{timestamp}{data}{previous_hash}"
         return hashlib.sha256(block_string.encode()).hexdigest()
 
     def validar_cadena(self):
+        """
+        Valida la cadena de bloques.
+        """
         for i in range(1, len(self.chain)):
             current_block = self.chain[i]
             previous_block = self.chain[i - 1]
@@ -53,6 +71,9 @@ class Blockchain:
         return True
 
     def imprimir_cadena(self):
+        """
+        Imprime la cadena de bloques.
+        """
         for block in self.chain:
             print(block)
 
@@ -62,4 +83,4 @@ if __name__ == "__main__":
     blockchain.agregar_bloque("Primer Bloque Después del Génesis")
     blockchain.agregar_bloque("Segundo Bloque Después del Génesis")
     blockchain.imprimir_cadena()
-    print("Cadena válida:", blockchain.validar_cadena()
+    print("Cadena válida:", blockchain.validar_cadena())
