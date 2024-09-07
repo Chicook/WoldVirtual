47a9854b2270e2c11ad0e669f717f9a113553192
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-07 03:25:09 +0200

    Update compresion.py

--- Metaverso_Crypto/inicio/Blockchain_Principal/compresion.py
+++ Metaverso_Crypto/inicio/Blockchain_Principal/compresion.py
@@ -1,5 +1,3 @@
-# modulo compresion #
-
 import json
 import gzip
 
@@ -44,28 +42,3 @@ def cargar_y_descomprimir_datos(archivo_entrada):
     except Exception as e:
         print(f"Error al cargar y descomprimir datos: {e}")
         return None
-
-# compresion.py
-
-# import json
-# import gzip
-
-# def comprimir_y_guardar_datos(datos, archivo_salida):
-    # try:
-       # datos_serializados = json.dumps(datos).encode('utf-8')
-       # datos_comprimidos = gzip.compress(datos_serializados)
-        # with open(archivo_salida, 'wb') as archivo:
-           # archivo.write(datos_comprimidos)
-        # print(f"Datos comprimidos y guardados en {archivo_salida}")
-    # except Exception as e:
-       #  print(f"Error al comprimir y guardar datos: {e}")
-
-# def cargar_y_descomprimir_datos(archivo_entrada):
-    # try:
-       #  with open(archivo_entrada, 'rb') as archivo:
-           # datos_comprimidos = archivo.read()
-      #  datos_descomprimidos = gzip.decompress(datos_comprimidos)
-      #  return json.loads(datos_descomprimidos)
-   # except Exception as e:
-       # print(f"Error al cargar y descomprimir datos: {e}")
-        # return None
