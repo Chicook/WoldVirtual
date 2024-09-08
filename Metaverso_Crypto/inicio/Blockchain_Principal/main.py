80b3a39f307f8409df8411b9c4d07ec837a61d67
Jaldvox <166617485+Jaldvox@users.noreply.github.com>
2024-09-08 22:58:51 +0200

    Update main.py

--- Metaverso_Crypto/inicio/Blockchain_Principal/main.py
+++ Metaverso_Crypto/inicio/Blockchain_Principal/main.py
@@ -10,7 +10,7 @@ from recursos import RecursosUsuario
 # from database import conectar_base_datos  # Comentado para futuras implementaciones
 from compresion import comprimir_y_guardar_datos
 from servidor import app, socketio
-from administración import Bksvmain
+from administración/Blockchain Servidor import Bksvmain
 
 # Cantidad total de tokens WCV
 TOTAL_WCV = 30000000.000
