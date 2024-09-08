d61ab10f089eb59d9f8142672720cb7831f6ef63
Jaldvox <166617485+Jaldvox@users.noreply.github.com>
2024-09-08 21:28:29 +0200

    Update Bksvutills.py

--- Metaverso_Crypto/inicio/Blockchain_Principal/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/Bksvutills.py
+++ Metaverso_Crypto/inicio/Blockchain_Principal/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/Bksvutills.py
@@ -5,9 +5,10 @@ from blockchain import Blockchain
 
 # Inicializar la blockchain
 blockchain = Blockchain()
+codigos_temporales = {}
 
 def log_action(data):
-    new_block = blockchain.crear_bloque(len(blockchain.chain), data, blockchain.chain[-1]['hash_admin'] if blockchain.chain else "0")
+    new_block = blockchain.new_block(len(blockchain.chain), data, blockchain.chain[-1]['previous_hash'] if blockchain.chain else "0")
     blockchain.chain.append(new_block)
     print(f"Acción registrada: {data}")
 
@@ -21,5 +22,4 @@ def verificar_codigo_temporal(codigo):
 
 def confirmar_conexion_modulos(modulos):
     data = f"Conexión de módulos: {', '.join(modulos)}"
-    blockchain.agregar_bloque(data)
-                                                
+    blockchain.new_block(data)
