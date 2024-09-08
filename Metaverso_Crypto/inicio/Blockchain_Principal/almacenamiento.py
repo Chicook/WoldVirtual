f8fb6d50d1105acbd2287430bfa93a982d026538
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-08 14:58:48 +0200

    Update almacenamiento.py

--- Metaverso_Crypto/inicio/Blockchain_Principal/almacenamiento.py
+++ Metaverso_Crypto/inicio/Blockchain_Principal/almacenamiento.py
@@ -1,8 +1,51 @@
 import os
 import tarfile
+import hashlib
+import time
 
 # Ruta donde se almacenan los archivos comprimidos y descomprimidos
 storage_path = '/workspaces/WoldVirtual.github.io/Metaverso_Crypto/inicio/Blockchain_Principal/Almacenamiento'
+
+class Block:
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        block_string = f"{self.index}{self.timestamp}{self.data}{self.previous_hash}"
+        return hashlib.sha256(block_string.encode()).hexdigest()
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+
+    def create_genesis_block(self):
+        return Block(0, time.time(), "Genesis Block", "0")
+
+    def get_latest_block(self):
+        return self.chain[-1]
+
+    def add_block(self, new_block):
+        new_block.previous_hash = self.get_latest_block().hash
+        new_block.hash = new_block.calculate_hash()
+        self.chain.append(new_block)
+
+# Inicializar la blockchain
+blockchain = Blockchain()
+
+def log_action(data):
+    """
+    Registra una acción en la blockchain.
+    
+    Args:
+        data (str): Descripción de la acción a registrar.
+    """
+    new_block = Block(len(blockchain.chain), time.time(), data, blockchain.get_latest_block().hash)
+    blockchain.add_block(new_block)
+    print(f"Acción registrada: {data}")
 
 def compress_files(files, output_filename):
     """
@@ -25,6 +68,7 @@ def compress_files(files, output_filename):
                 tar.add(file, arcname=os.path.basename(file))
     
     print(f'Archivos comprimidos en {output_filename}, guardado en {output_filepath}')
+    log_action(f"Comprimidos archivos en {output_filename}")
 
 def decompress_file(input_filename):
     """
@@ -44,4 +88,12 @@ def decompress_file(input_filename):
         tar.extractall(path=storage_path)
     
     print(f'Archivos descomprimidos en {storage_path}')
-    
+    log_action(f"Descomprimido archivo {input_filename}")
+
+# Ejemplo de uso
+compress_files(['file1.txt', 'file2.txt'], 'output.tar.gz')
+decompress_file('output.tar.gz')
+
+# Mostrar la cadena de bloques
+for block in blockchain.chain:
+    print(f"Índice: {block.index}, Hash: {block.hash}, Datos: {block.data}")
