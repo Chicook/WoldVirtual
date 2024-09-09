f6fc865353fe245fd0a08461321f58cc1173b59b
Jaldvox <166617485+Jaldvox@users.noreply.github.com>
2024-09-09 22:42:26 +0200

    Update prb2.py

--- Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/prb2.py
+++ Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/prb2.py
@@ -1,18 +1,13 @@
 import hashlib
-from prb5 import blockchain
+import prb4
 
-# Usuarios registrados (simulación)
 usuarios = {}
 
 def registrar_usuario(username, password, wallet_id):
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
     usuarios[username] = {'password': hashed_password, 'wallet': wallet_id}
-    registrar_actividad_css(f"Usuario registrado: {username}")
+    prb4.registrar_actividad_css(f"Usuario registrado: {username}")
 
 def verificar_credenciales(username, password):
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
     return usuarios.get(username, {}).get('password') == hashed_password
-
-def registrar_actividad_css(actividad):
-    new_block = {'index': len(blockchain) + 1, 'data': actividad, 'hash': hashlib.sha256(actividad.encode()).hexdigest()}
-    blockchain.append(new_block)
