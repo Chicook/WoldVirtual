8ed22e1ea659013074d3e936bddaf5ab073df2ab
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-09 10:54:36 +0000

    add

---
+++ Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/prb4.py
@@ -0,0 +1,21 @@
+@app.route('/blockchain', methods=['GET'])
+def get_blockchain():
+    return jsonify({'blockchain': blockchain})
+
+    @app.route('/add_block', methods=['POST'])
+    def add_block():
+        data = request.get_json()
+            if 'data' in data:
+                    new_block = {'index': len(blockchain) + 1, 'data': data['data']}
+                            blockchain.append(new_block)
+                                    return jsonify({'message': 'Bloque agregado correctamente', 'block': new_block})
+                                        else:
+                                                return jsonify({'error': 'Datos no proporcionados'})
+
+                                                @app.route('/block/<int:block_index>', methods=['GET'])
+                                                def get_block(block_index):
+                                                    if 0 < block_index <= len(blockchain):
+                                                            return jsonify({'block': blockchain[block_index - 1]})
+                                                                else:
+                                                                        return jsonify({'error': 'Índice de bloque inválido'})
+                                                                        
