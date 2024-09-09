1737a853f722e23d51137929355cd0f190254795
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-09 20:25:12 +0200

    Update prb3.py

--- Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/prb3.py
+++ Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/prb3.py
@@ -1,7 +1,8 @@
+import random
+
+def generar_codigo_temporal():
+    return random.randint(100000, 999999)
+
 def manejar_accion(username, accion):
     # Código para manejar acciones de usuario
-    pass
-
-def registrar_actividad_js(mensaje):
-    # Código para registrar actividad en JavaScript
     pass
