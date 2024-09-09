cccfeb5c54ae53cc386905a56be09dc64d86ec64
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-09 20:12:28 +0200

    Update prb4.py

--- Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/prb4.py
+++ Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/prb4.py
@@ -1,3 +1,12 @@
-def aplicar_estilos():
-    # Código para aplicar estilos CSS
+blockchain = []
+
+def get_blockchain():
+    return blockchain
+
+def add_block():
+    # Código para añadir un bloque a la blockchain
+    pass
+
+def get_block(block_index):
+    # Código para obtener un bloque específico de la blockchain
     pass
