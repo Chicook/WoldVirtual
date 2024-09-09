bbbf6eff1b37b1cdde45c920d12cc91b2fb03cd4
Jaldvox <166617485+Jaldvox@users.noreply.github.com>
2024-09-09 21:40:57 +0200

    Update prb3.py

--- Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/prb3.py
+++ Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/prb3.py
@@ -1,29 +1,8 @@
-import random
-from prb2 import registrar_actividad_css
+import uuid
+import prb5
 
 def generar_codigo_temporal():
-    """
-    Genera un código temporal de 6 dígitos.
-    
-    Returns:
-        int: Código temporal de 6 dígitos.
-    """
-    return random.randint(100000, 999999)
+    return str(uuid.uuid4())
 
-def manejar_accion(usuario, accion):
-    """
-    Maneja las acciones del usuario y registra la actividad en la blockchain.
-    
-    Args:
-        usuario (str): Nombre del usuario.
-        accion (str): Acción a realizar (explorar/intercambiar).
-    """
-    if accion == "explorar":
-        print(f"Bienvenido/a {usuario} al entorno de exploración.")
-    elif accion == "intercambiar":
-        print(f"Realizando intercambio para {usuario}.")
-    else:
-        print("Acción no reconocida.")
-    
-    # Registrar la actividad en la blockchain
-    registrar_actividad_css(f"Acción realizada: {accion} por {usuario}")
+def manejar_accion(nombre_usuario, accion):
+    prb5.registrar_en_blockchain(f"Acción realizada: {accion} por {nombre_usuario}")
