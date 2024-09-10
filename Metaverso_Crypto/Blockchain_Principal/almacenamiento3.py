888ec85bdf861aca977b8768451e50960a118f3b
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-10 16:31:41 +0200

    Update almacenamiento3.py

--- Metaverso_Crypto/Blockchain_Principal/almacenamiento3.py
+++ Metaverso_Crypto/Blockchain_Principal/almacenamiento3.py
@@ -1,1 +1,12 @@
+class User:
+    def __init__(self, user_id, wallet):
+        self.user_id = user_id
+        self.wallet = wallet
 
+    def send_wcv(self, amount, recipient_wallet):
+        # Lógica para enviar WCV
+        print(f"{self.user_id} envió {amount} WCV a {recipient_wallet}")
+
+    def receive_wcv(self, amount, sender_wallet):
+        # Lógica para recibir WCV
+        print(f"{self.user_id} recibió {amount} WCV de {sender_wallet}")
