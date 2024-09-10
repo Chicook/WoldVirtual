01d69a4034c2abae5ec796619ae60dfaf7d7bd64
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-10 13:02:08 +0200

    Update SBlokchain4.py

--- Metaverso_Crypto/Blockchain_Principal/SBlokchain4.py
+++ Metaverso_Crypto/Blockchain_Principal/SBlokchain4.py
@@ -1,1 +1,8 @@
+from web3 import Web3
 
+def connect_to_web3():
+    web3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
+    if web3.isConnected():
+        print("Connected to Ethereum node")
+    else:
+        print("Failed to connect")
