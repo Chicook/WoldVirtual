74396e3ca0fd51265275ac77e7bbffa2781cbfec
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-10 12:25:50 +0200

    Update prb4.py

--- Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/prb4.py
+++ Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/prb4.py
@@ -1,15 +1,8 @@
-from vpython import vec, sphere, color
+from web3 import Web3
 
-def create_sphere():
-    sphere_pos = vec(0, 0, 0)
-    return sphere(pos=sphere_pos, radius=1, color=color.red)
-
-def create_block(module_name, previous_hash):
-    import hashlib
-    block_data = f'{module_name}-{previous_hash}'
-    block_hash = hashlib.sha256(block_data.encode()).hexdigest()
-    return {'module': module_name, 'hash': block_hash, 'previous_hash': previous_hash}
-
-def validate_block(block):
-    print(f"Validating block: {block['module']} with hash: {block['hash']}")
-    
+def connect_to_web3():
+    web3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
+    if web3.isConnected():
+        print("Connected to Ethereum node")
+    else:
+        print("Failed to connect")
