c411cdc4d7fcbffc78c68ef8fa58214d75c6b962
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-10 16:56:21 +0200

    Update prb2.py

--- Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/prb2.py
+++ Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/prb2.py
@@ -1,5 +1,5 @@
+# prb2.py
 import hashlib
-import time
 
 class Block:
     def __init__(self, index, timestamp, data, previous_hash):
@@ -15,15 +15,16 @@ class Block:
 
 class Blockchain:
     def __init__(self):
-        self.chain = [self.create_genesis_block()]
-
-    def create_genesis_block(self):
-        return Block(0, time.time(), "Genesis Block", "0")
+        self.chain = []
 
     def get_latest_block(self):
-        return self.chain[-1]
+        return self.chain[-1] if self.chain else None
 
     def add_block(self, new_block):
-        new_block.previous_hash = self.get_latest_block().hash
+        if self.chain:
+            new_block.previous_hash = self.get_latest_block().hash
+        else:
+            new_block.previous_hash = "0"
         new_block.hash = new_block.calculate_hash()
         self.chain.append(new_block)
+        
