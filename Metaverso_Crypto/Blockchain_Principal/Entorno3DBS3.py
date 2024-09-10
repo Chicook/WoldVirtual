e9185d9a086c4b3c0e46ee8e04e9335f1eea8bf0
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-10 11:59:58 +0200

    Update Entorno3DBS3.py

--- Metaverso_Crypto/Blockchain_Principal/Entorno3DBS3.py
+++ Metaverso_Crypto/Blockchain_Principal/Entorno3DBS3.py
@@ -1,29 +1,29 @@
-from vpython import vec, cylinder, pi, cos, sin
+# from vpython import vec, cylinder, pi, cos, sin
 
-def create_structure():
-    radius = 10
-    num_spaces = 6
-    space_width = 2
+# def create_structure():
+   #  radius = 10
+   # num_spaces = 6
+   # space_width = 2
 
-    structure = []
-    for i in range(num_spaces):
-        theta = i * 2 * pi / num_spaces
-        x = radius * cos(theta)
-        y = radius * sin(theta)
-        space = cylinder(pos=vec(x, y, 0), 
-                         axis=vec(0, 0, space_width), 
-                         radius=space_width / 2)
-        structure.append(space)
+   # structure = []
+   # for i in range(num_spaces):
+       # theta = i * 2 * pi / num_spaces
+       # x = radius * cos(theta)
+       # y = radius * sin(theta)
+       # space = cylinder(pos=vec(x, y, 0), 
+                         # axis=vec(0, 0, space_width), 
+                        # radius=space_width / 2)
+      #  structure.append(space)
     
-    return structure
+   # return structure
 
-def create_block(module_name, previous_hash):
-    import hashlib
-    block_data = f'{module_name}-{previous_hash}'
-    block_hash = hashlib.sha256(block_data.encode()).hexdigest()
-    return {'module': module_name, 'hash': block_hash, 'previous_hash': 
-            previous_hash}
+# def create_block(module_name, previous_hash):
+   # import hashlib
+    # block_data = f'{module_name}-{previous_hash}'
+   # block_hash = hashlib.sha256(block_data.encode()).hexdigest()
+    # return {'module': module_name, 'hash': block_hash, 'previous_hash': 
+          #  previous_hash}
 
-def validate_block(block):
-    print(f"Validating block: {block['module']} 
-    with hash: {block['hash']}")
+# def validate_block(block):
+    # print(f"Validating block: {block['module']} 
+   #  with hash: {block['hash']}")
