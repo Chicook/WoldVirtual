580e89b53ec4a7d9127cf639af669efe95c70f79
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-10 20:50:58 +0200

    Update prb1.py

--- Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/prb1.py
+++ Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/prb1.py
@@ -1,42 +1,35 @@
-from prb2 import gestionar_usuarios, calcular_hash, crear_etiqueta
-from prb3 import gestionar_recursos, validar_bloque, crear_entrada
-from prb4 import comunicacion_nodos, crear_etiqueta_nombre, crear_entrada_nombre, crear_etiqueta_descripcion, crear_entrada_descripcion
-from prb5 import iniciar_interfaz, compartir_recurso
-from web3 import Web3
-
-class Blockchain:
-    def __init__(self):
-        self.chain = []
-        self.create_block(data="Bloque inicial", previous_hash='0')  # Bloque inicial, no génesis
-
-    def create_block(self, data, previous_hash):
-        block = {'index': len(self.chain) + 1,
-                 'data': data,
-                 'previous_hash': previous_hash,
-                 'hash': calcular_hash(data, previous_hash)}
-        return block
-
-    def get_previous_block(self):
-        return self.chain[-1]
-
-    def add_block(self, data):
-        previous_block = self.get_previous_block()
-        previous_hash = previous_block['hash']
-        block = self.create_block(data, previous_hash)
-        if validar_bloque(block):
-            self.chain.append(block)
-        return block
-
-blockchain = Blockchain()
+from prb2 import TokenICO
+from prb3 import PixlNFT
+from prb4 import WoldcoinVirtual
+from prb5 import Blockchain, Block
+import time
 
 def main():
-    data = {
-        'usuarios': gestionar_usuarios(),
-        'recursos': gestionar_recursos(),
-        'comunicacion': comunicacion_nodos(),
-        'interfaz': iniciar_interfaz()
-    }
-    blockchain.add_block(data)
+    # Inicializar blockchain
+    blockchain = Blockchain()
+
+    # Ejemplo de uso de TokenICO
+    ico = TokenICO(1000)
+    ico.purchase_tokens("buyer_address", 10, 10)
+    ico.withdraw_funds()
+    blockchain.add_block(Block(1, blockchain.get_latest_block_hash(), "TokenICO Transactions", time.time()))
+
+    # Ejemplo de uso de PixlNFT
+    nft = PixlNFT()
+    nft.create_token("creator_address", "token_uri_example", 1)
+    blockchain.add_block(Block(2, blockchain.get_latest_block_hash(), "PixlNFT Transactions", time.time()))
+
+    # Ejemplo de uso de WoldcoinVirtual
+    wcv = WoldcoinVirtual()
+    wcv.transfer("owner_address", "recipient_address", 1000)
+    wcv.stake("owner_address", 500)
+    wcv.unstake("owner_address", 500)
+    blockchain.add_block(Block(3, blockchain.get_latest_block_hash(), "WoldcoinVirtual Transactions", time.time()))
+
+    # Verificar la validez de la blockchain
+    print("Blockchain válida:", blockchain.is_chain_valid())
+    for block in blockchain.chain:
+        print(f"Index: {block.index}, Hash: {block.hash}, Previous Hash: {block.previous_hash}, Transactions: {block.transactions}")
 
 if __name__ == "__main__":
     main()
