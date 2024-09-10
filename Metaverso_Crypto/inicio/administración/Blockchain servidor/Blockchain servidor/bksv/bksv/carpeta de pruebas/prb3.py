0197f369f6910568acbed3b949a8df62f85f7782
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-10 20:53:47 +0200

    Update prb3.py

--- Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/prb3.py
+++ Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/prb3.py
@@ -1,16 +1,13 @@
-from prb2 import calcular_hash
-import tkinter as tk
+class PixlNFT:
+    def __init__(self):
+        self.token_ids = 0
+        self.tokens = {}
 
-def gestionar_recursos():
-    # Lógica de gestión de recursos
-    return "Datos de recursos"
-
-def validar_bloque(block):
-    # Valida el bloque
-    expected_hash = calcular_hash(block['data'], block['previous_hash'])
-    return block['hash'] == expected_hash
-
-def crear_entrada(master):
-    entrada = tk.Entry(master)
-    entrada.pack()
-    return entrada
+    def create_token(self, caller_address, token_uri, value):
+        if value < 1:
+            raise ValueError("Not enough ETH sent; check price!")
+        self.token_ids += 1
+        new_item_id = self.token_ids
+        self.tokens[new_item_id] = {"owner": caller_address, "uri": token_uri}
+        print(f"Token created: {new_item_id}, {token_uri}")
+        return new_item_id
