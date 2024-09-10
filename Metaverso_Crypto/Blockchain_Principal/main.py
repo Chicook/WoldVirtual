31321ea9ae8b38b388a04fb569a21e1e91ae9049
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-10 13:07:39 +0200

    Update main.py

--- Metaverso_Crypto/Blockchain_Principal/main.py
+++ Metaverso_Crypto/Blockchain_Principal/main.py
@@ -10,6 +10,9 @@ from recursos import RecursosUsuario
 # from database import conectar_base_datos  # Comentado para futuras implementaciones
 from compresion import comprimir_y_guardar_datos
 from servidor import app, socketio
+# from 3D1 
+# from 
+# from 
 
 # Cantidad total de tokens WCV
 TOTAL_WCV = 30000000.000
