13be8cb31572f622d3069aeb6d6a80fae53db96f
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-10 11:57:15 +0200

    Update BSnts.py

--- Metaverso_Crypto/Blockchain_Principal/BSnts.py
+++ Metaverso_Crypto/Blockchain_Principal/BSnts.py
@@ -1,12 +1,12 @@
-blockchain = []
+# blockchain = []
 
-def obtener_blockchain():
-    return blockchain
+# def obtener_blockchain():
+    # return blockchain
 
-def agregar_bloque(data):
-    new_block = {'index': len(blockchain) + 1, 'data': data}
-    blockchain.append(new_block)
-    return new_block
+# def agregar_bloque(data):
+   #  new_block = {'index': len(blockchain) + 1, 'data': data}
+   #  blockchain.append(new_block)
+   #  return new_block
 
 def obtener_bloque(block_index):
     if 0 < block_index <= len(blockchain):
