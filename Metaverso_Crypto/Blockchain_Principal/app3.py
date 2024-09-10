2310d4fe5aedb62153aff1e11394f9cd1f39650c
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-10 17:10:22 +0200

    Update app3.py

--- Metaverso_Crypto/Blockchain_Principal/app3.py
+++ Metaverso_Crypto/Blockchain_Principal/app3.py
@@ -1,1 +1,9 @@
+import time
+from app2 import Block, Blockchain
 
+blockchain = Blockchain()
+
+def log_action(data):
+    new_block = Block(len(blockchain.chain), time.time(), data, blockchain.get_latest_block().hash if blockchain.chain else "0")
+    blockchain.add_block(new_block)
+    print(f"Acción registrada: {data}")
