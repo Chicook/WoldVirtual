ce6a751d78701abe721cfe8de8d5a6f90f8f64ed
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-10 16:59:32 +0200

    Update prb4.py

--- Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/prb4.py
+++ Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/prb4.py
@@ -1,50 +1,20 @@
+# prb4.py
 import os
 import tarfile
-import prb2
+from prb3 import log_action
 
-# Ruta donde se almacenan los archivos comprimidos y descomprimidos
 storage_path = '/workspaces/WoldVirtual.github.io/Metaverso_Crypto/inicio/Blockchain_Principal/Almacenamiento'
 
-def compress_files(files, output_filename):
-    """
-    Comprime una lista de archivos en un archivo tar.gz.
-    
-    Args:
-        files (list): Lista de rutas de archivos a comprimir.
-        output_filename (str): Nombre del archivo tar.gz de salida.
-    """
-    output_filepath = os.path.join(storage_path, output_filename)
-    
-    # Verifica que los archivos existan antes de intentar comprimirlos
-    for file in files:
-        if not os.path.isfile(file):
-            print(f"Advertencia: El archivo {file} no existe y no será incluido en la compresión.")
-    
+def comprimir_datos(datos, archivo):
+    output_filepath = os.path.join(storage_path, archivo)
     with tarfile.open(output_filepath, 'w:gz') as tar:
-        for file in files:
-            if os.path.isfile(file):  # Solo añade los archivos existentes
+        for file in datos:
+            if os.path.isfile(file):
                 tar.add(file, arcname=os.path.basename(file))
-    
-    print(f'Archivos comprimidos en {output_filename}, guardado en {output_filepath}')
-    prb2.log_action(f"Comprimidos archivos en {output_filename}")
+    log_action(f"Datos comprimidos en {archivo}")
 
-def decompress_file(input_filename):
-    """
-    Descomprime un archivo tar.gz en el directorio de almacenamiento.
-
-    Args:
-        input_filename (str): Nombre del archivo tar.gz a descomprimir.
-    """
-    input_filepath = os.path.join(storage_path, input_filename)
-    
-    # Verifica que el archivo de entrada exista antes de intentar descomprimirlo
-    if not os.path.isfile(input_filepath):
-        print(f"Error: El archivo {input_filepath} no existe.")
-        return
-    
+def descomprimir_datos(archivo):
+    input_filepath = os.path.join(storage_path, archivo)
     with tarfile.open(input_filepath, 'r:gz') as tar:
         tar.extractall(path=storage_path)
-    
-    print(f'Archivos descomprimidos en {storage_path}')
-    prb2.log_action(f"Descomprimido archivo {input_filename}")
-    
+    log_action(f"Datos descomprimidos de {archivo}")
