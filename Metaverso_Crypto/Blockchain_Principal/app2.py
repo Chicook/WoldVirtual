f9231273ad10293edffb9930c6b0694e50089418
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-10 17:08:17 +0200

    Update app2.py

--- Metaverso_Crypto/Blockchain_Principal/app2.py
+++ Metaverso_Crypto/Blockchain_Principal/app2.py
@@ -1,1 +1,28 @@
+import hashlib
 
+class Block:
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        block_string = f"{self.index}{self.timestamp}{self.data}{self.previous_hash}"
+        return hashlib.sha256(block_string.encode()).hexdigest()
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+
+    def get_latest_block(self):
+        return self.chain[-1] if self.chain else None
+
+    def add_block(self, new_block):
+        if self.chain:
+            new_block.previous_hash = self.get_latest_block().hash
+        else:
+            new_block.previous_hash = "0"
+        new_block.hash = new_block.calculate_hash()
+        self.chain.append(new_block)
