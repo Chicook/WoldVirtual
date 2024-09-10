2e07ef0716c7a74a822e66e11af6f5b0f1b2397b
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-10 18:52:38 +0200

    Update prb4.py

--- Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/prb4.py
+++ Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/prb4.py
@@ -1,10 +1,25 @@
-# prb4.py
-from prb2 import log_action
+import tkinter as tk
 
-def procesar_transaccion(transaccion):
-    log_action(f"Procesada transacción: {transaccion}")
+def comunicacion_nodos():
+    # Lógica de comunicación entre nodos
+    return "Datos de comunicación"
 
-def validar_transaccion(transaccion):
-    es_valida = True  # Supongamos que la validación es exitosa
-    log_action(f"Transacción validada: {transaccion} - Válida: {es_valida}")
-    return es_valida
+def crear_etiqueta_nombre(master):
+    etiqueta_nombre = tk.Label(master, text="Nombre:")
+    etiqueta_nombre.pack()
+    return etiqueta_nombre
+
+def crear_entrada_nombre(master):
+    entry_nombre = tk.Entry(master)
+    entry_nombre.pack()
+    return entry_nombre
+
+def crear_etiqueta_descripcion(master):
+    etiqueta_descripcion = tk.Label(master, text="Descripción:")
+    etiqueta_descripcion.pack()
+    return etiqueta_descripcion
+
+def crear_entrada_descripcion(master):
+    entry_descripcion = tk.Entry(master)
+    entry_descripcion.pack()
+    return entry_descripcion
