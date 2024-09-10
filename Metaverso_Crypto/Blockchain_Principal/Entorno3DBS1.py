55ffbd53c3abef49704c56ac35e9a5c156bc95cb
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-10 09:42:01 +0200

    Update Entorno3DBS1.py

--- Metaverso_Crypto/Blockchain_Principal/Entorno3DBS1.py
+++ Metaverso_Crypto/Blockchain_Principal/Entorno3DBS1.py
@@ -1,30 +1,29 @@
-from vpython import *
+# from vpython import *
+# from Entorno3DBS2 import create_scene
+# from Entorno3DBS3 import create_structure
+# from Entorno3DBS4 import create_sphere
+# from Entorno3DBS5 import animate_sphere, create_block, validate_block
 
 # Crear la escena
-scene = canvas(title='Estación Espacial', width=800, height=600)
+# scene = create_scene()
 
 # Crear la estructura de la estación espacial
-radius = 10
-num_spaces = 6
-space_width = 2
-
-structure = []
-for i in range(num_spaces):
-    theta = i * 2*pi/num_spaces
-    x = radius*cos(theta)
-    y = radius*sin(theta)
-    space = cylinder(pos=vec(x, y, 0), axis=vec(0, 0, space_width), radius=space_width/2)
-    structure.append(space)
+# structure = create_structure()
 
 # Crear la esfera animada
-sphere_pos = vec(0, 0, 0)
-sphere = sphere(pos=sphere_pos, radius=1, color=color.red)
-
-def animate_sphere():
-    rate(30)
-    sphere.rotate(angle=pi/180, axis=vec(1, 0, 0))
-    if scene.current_time < 70:
-        scene.after(animate_sphere)
+# sphere = create_sphere()
 
 # Animar la esfera
-animate_sphere()
+# animate_sphere(sphere)
+
+# Crear y validar bloques en la blockchain
+# block1 = create_block('Entorno3DBS1', 'Genesis Block')
+# block2 = create_block('Entorno3DBS2', block1['hash'])
+# block3 = create_block('Entorno3DBS3', block2['hash'])
+# block4 = create_block('Entorno3DBS4', block3['hash'])
+# block5 = create_block('Entorno3DBS5', block4['hash'])
+
+# blocks = [block1, block2, block3, block4, block5]
+
+# for block in blocks:
+    # validate_block(block)
