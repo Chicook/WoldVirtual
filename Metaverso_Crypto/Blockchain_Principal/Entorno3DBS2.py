b7180c2c0abd656615537ba167cb130325ec25b8
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-10 11:58:18 +0200

    Update Entorno3DBS2.py

--- Metaverso_Crypto/Blockchain_Principal/Entorno3DBS2.py
+++ Metaverso_Crypto/Blockchain_Principal/Entorno3DBS2.py
@@ -1,13 +1,13 @@
-from vpython import canvas
+# from vpython import canvas
 
-def create_scene():
-    return canvas(title='Estación Espacial', width=800, height=600)
+# def create_scene():
+    # return canvas(title='Estación Espacial', width=800, height=600)
 
-def create_block(module_name, previous_hash):
-    import hashlib
-    block_data = f'{module_name}-{previous_hash}'
-    block_hash = hashlib.sha256(block_data.encode()).hexdigest()
-    return {'module': module_name, 'hash': block_hash, 'previous_hash': previous_hash}
+# def create_block(module_name, previous_hash):
+   #  import hashlib
+   #  block_data = f'{module_name}-{previous_hash}'
+   #  block_hash = hashlib.sha256(block_data.encode()).hexdigest()
+   #  return {'module': module_name, 'hash': block_hash, 'previous_hash': previous_hash}
 
-def validate_block(block):
-    print(f"Validating block: {block['module']} with hash: {block['hash']}")
+# def validate_block(block):
+    # print(f"Validating block: {block['module']} with hash: {block['hash']}")
