e7b56d34f5ae25d362516c03b33638ffb9e4a87b
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-10 17:50:39 +0200

    Update avatarbs4.py

--- Metaverso_Crypto/Blockchain_Principal/avatarbs4.py
+++ Metaverso_Crypto/Blockchain_Principal/avatarbs4.py
@@ -1,1 +1,10 @@
+# prb4.py
+from prb2 import log_action
 
+def procesar_transaccion(transaccion):
+    log_action(f"Procesada transacción: {transaccion}")
+
+def validar_transaccion(transaccion):
+    es_valida = True  # Supongamos que la validación es exitosa
+    log_action(f"Transacción validada: {transaccion} - Válida: {es_valida}")
+    return es_valida
