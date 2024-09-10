0bfa04d29efd50f90bce406f31610a845ec52137
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-10 11:14:40 +0200

    Update Entorno3DBS5.py

--- Metaverso_Crypto/Blockchain_Principal/Entorno3DBS5.py
+++ Metaverso_Crypto/Blockchain_Principal/Entorno3DBS5.py
@@ -1,1 +1,20 @@
+from vpython import rate, pi, vec
 
+def animate_sphere(sphere):
+    def animation():
+        rate(30)
+        sphere.rotate(angle=pi/180, axis=vec(1, 0, 0))
+        if scene.current_time < 70:
+            scene.after(animation)
+    
+    animation()
+
+def create_block(module_name, previous_hash):
+    import hashlib
+    block_data = f'{module_name}-{previous_hash}'
+    block_hash = hashlib.sha256(block_data.encode()).hexdigest()
+    return {'module': module_name, 'hash': block_hash, 'previous_hash': previous_hash}
+
+def validate_block(block):
+    print(f"Validating block: {block['module']} with hash: {block['hash']}")
+    
