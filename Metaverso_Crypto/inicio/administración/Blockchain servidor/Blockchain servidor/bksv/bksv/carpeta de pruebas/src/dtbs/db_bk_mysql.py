2b36da1c95f0f4cd614a0d5ce611e9057be371bb
Jaldvox <166617485+Jaldvox@users.noreply.github.com>
2024-09-11 14:41:34 +0000

    msql

--- Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/src/dtbs/db_bk_mysql.py
+++ Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/src/dtbs/db_bk_mysql.py
@@ -1,1 +1,16 @@
 ## desarrollar mas adelante MySQL en desarrollo una blockchain con su base de datos interna ##
+
+# from decouple import config 
+# import pymysql 
+# 
+# def get_connetion():
+#     try: 
+#        return pymysql.connet(
+#            host=config('MYSQL_HOST'),
+#            user=config('MYSQL_USER'),
+#            password=config('MYSQL_PASSWORD'),
+#            db=config('MYSQL_DB'),
+#        )
+#     except Exception as ex:
+#         print(ex)
+#
