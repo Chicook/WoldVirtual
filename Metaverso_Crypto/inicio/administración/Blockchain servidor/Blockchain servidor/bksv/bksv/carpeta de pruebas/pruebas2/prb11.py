694d609634107930de650b22f2a6bb01749bb217
Jaldvox <166617485+Jaldvox@users.noreply.github.com>
2024-09-11 09:54:34 +0000

    add

--- Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/pruebas2/prb11.py
+++ Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/pruebas2/prb11.py
@@ -1,1 +1,5 @@
-##pruebas 
+## pruebas modulo principall ##
+# from  prb12 import 'funcion o funciones del modulo prb12' 
+# from  prb13 import 'funcion o funciones del modulo prb13'
+# from  prb14 import 'funcion o funciones del modulo prb14'
+# from  prb15 import 'funcion o funciones del modulo prb15'
