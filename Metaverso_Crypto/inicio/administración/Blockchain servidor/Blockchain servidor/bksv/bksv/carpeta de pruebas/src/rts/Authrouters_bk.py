d8446d3068e63202f3d9802e5c75d8fc2eb893ba
Jaldvox <166617485+Jaldvox@users.noreply.github.com>
2024-09-11 15:30:44 +0000

    add

--- Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/src/rts/Authrouters_bk.py
+++ Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/src/rts/Authrouters_bk.py
@@ -1,1 +1,18 @@
-##
+# from src.utils.Security import Security 
+# service 
+# from src.services.authservice import AuthService 
+# 
+# main = Blueprint('auth_blueprint', __name__)
+
+
+# @main.route('/', methods=['POST'])
+# def login():
+#     try:
+#        username = request.json['username']
+#        password = resquest.json['password']
+#
+#        _user = User(0, username, password, None)
+#        authenticate_user = AuthService.login_user(_user)
+#
+#        if (authenticated_user = None):
+#
