363d19dc6c66f04cbf2f0648d766503d258939dd
Jaldvox <166617485+Jaldvox@users.noreply.github.com>
2024-09-11 18:03:46 +0200

    Update Bkdtbs5.py

--- Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/src/dtbs/Bkdtbs5.py
+++ Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/src/dtbs/Bkdtbs5.py
@@ -1,1 +1,11 @@
+# prb5.py
+from prb4 import hash_block
+
+def register_user(blockchain, user):
+    block = {
+        'username': user.username,
+        'previous_hash': hash_block(blockchain.chain[-1]) if blockchain.chain else '0'
+    }
+    block['hash'] = hash_block(block)
+    blockchain.add_block(block)
 
