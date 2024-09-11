4eb05997551e9a923909436f2553afcc3450aa0e
Jaldvox <166617485+Jaldvox@users.noreply.github.com>
2024-09-12 00:01:01 +0200

    Update prb5.py

--- Metaverso_Crypto/inicio/Zona_depruebas/webadministrativa/Blockchain_Principal/Zona_de_pruebas/admin/pruebas2/prb5.py
+++ Metaverso_Crypto/inicio/Zona_depruebas/webadministrativa/Blockchain_Principal/Zona_de_pruebas/admin/pruebas2/prb5.py
@@ -1,1 +1,19 @@
+# prb5.py
+import pygame
+from OpenGL.GL import *
+from prb4 import create_and_validate_block
+
+def main_loop(draw_function):
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        glRotatef(1, 3, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        draw_function()
+        pygame.display.flip()
+        pygame.time.wait(10)
+        create_and_validate_block()
 
