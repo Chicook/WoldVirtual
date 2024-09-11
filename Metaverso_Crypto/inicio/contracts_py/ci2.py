f2180ef64e79fd8af8c008fe643736849f1849e8
Jaldvox <166617485+Jaldvox@users.noreply.github.com>
2024-09-11 12:16:12 +0200

    Update ci2.py

--- Metaverso_Crypto/inicio/contracts_py/ci2.py
+++ Metaverso_Crypto/inicio/contracts_py/ci2.py
@@ -1,1 +1,33 @@
-## pruebas
+from datetime import datetime, timedelta
+
+class LiquidityPool:
+    def __init__(self, token1, token2):
+        self.token1 = token1
+        self.token2 = token2
+        self.totalSupply = 0
+        self.balances = {}
+        self.minTimeBetweenTransactions = timedelta(minutes=5)
+        self.lastTransactionTime = datetime.now()
+
+    def add_liquidity(self, user, amount1, amount2):
+        pass
+
+    def remove_liquidity(self, user, liquidity):
+        pass
+
+    def buy_tokens(self, user, amount):
+        pass
+
+    def sell_tokens(self, user, tokens):
+        pass
+
+    def get_balance(self, user):
+        return self.balances.get(user, 0)
+
+    def get_pool_state(self):
+        return {
+            "totalSupply": self.totalSupply,
+            "token1_balance": self.token1.balance_of(self),
+            "token2_balance": self.token2.balance_of(self),
+            "lastTransactionTime": self.lastTransactionTime
+        }
