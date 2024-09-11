a2d97af2196f75c249b651f6ba17e1e9ba7cebd3
Jaldvox <166617485+Jaldvox@users.noreply.github.com>
2024-09-11 15:38:06 +0000

    add

--- Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/src/mdls/lenguajemodels_bk.py
+++ Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/src/mdls/lenguajemodels_bk.py
@@ -10,3 +10,14 @@
 #           'name' : self.name
 #
 # }
+class Language():
+
+        def __init__(self, id, name) -> None:
+                self.id = id
+                        self.name = name
+
+                            def to_json(self):
+                                    return {
+                                                'id': self.id,
+                                                            'name': self.name
+                                                                    }
