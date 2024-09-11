5c7c81c77e72f784f318f138fe4ca5fcbd804443
Jaldvox <166617485+Jaldvox@users.noreply.github.com>
2024-09-11 15:02:08 +0000

    add

--- Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/src/mdls/usermdls_bk.py
+++ Metaverso_Crypto/inicio/administración/Blockchain servidor/Blockchain servidor/bksv/bksv/carpeta de pruebas/src/mdls/usermdls_bk.py
@@ -1,1 +1,7 @@
-##
+# Class User():
+#
+#    def __ini__(self, id, username, fullname) none:
+#        self.id = id 
+#        self.username = username 
+#        self.password = password 
+#        self.fullname = fullname 
