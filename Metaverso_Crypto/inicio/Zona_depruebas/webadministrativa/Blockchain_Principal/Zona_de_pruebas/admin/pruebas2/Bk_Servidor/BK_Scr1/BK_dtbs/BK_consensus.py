c60a8b03f1e4a1a32daa8a0114089cae158a2968
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-12 22:43:27 +0200

    Update BK_consensus.py

--- Metaverso_Crypto/inicio/Zona_depruebas/webadministrativa/Blockchain_Principal/Zona_de_pruebas/admin/pruebas2/Bk_Servidor/BK_Scr1/BK_dtbs/BK_consensus.py
+++ Metaverso_Crypto/inicio/Zona_depruebas/webadministrativa/Blockchain_Principal/Zona_de_pruebas/admin/pruebas2/Bk_Servidor/BK_Scr1/BK_dtbs/BK_consensus.py
@@ -1,1 +1,7 @@
+class Consensus:
+    def __init__(self, blockchain):
+        self.blockchain = blockchain
 
+    def validate_chain(self):
+        # Implementa la lógica de validación de la cadena aquí
+        pass
