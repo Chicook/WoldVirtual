c4aaf0b17050a56b82f96c9a942c453d2d213833
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-12 22:51:54 +0200

    Update BK_minig.py

--- Metaverso_Crypto/inicio/Zona_depruebas/webadministrativa/Blockchain_Principal/Zona_de_pruebas/admin/pruebas2/Bk_Servidor/BK_Scr1/BK_dtbs/BK_minig.py
+++ Metaverso_Crypto/inicio/Zona_depruebas/webadministrativa/Blockchain_Principal/Zona_de_pruebas/admin/pruebas2/Bk_Servidor/BK_Scr1/BK_dtbs/BK_minig.py
@@ -1,1 +1,8 @@
+class Miner:
+    def __init__(self, blockchain):
+        self.blockchain = blockchain
 
+    def mine_block(self, data):
+        # Implementa la lógica de minería aquí
+        pass
+      
