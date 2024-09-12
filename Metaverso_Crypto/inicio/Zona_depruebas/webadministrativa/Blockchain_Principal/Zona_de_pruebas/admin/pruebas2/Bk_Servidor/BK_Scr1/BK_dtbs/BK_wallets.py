33b0d6aeda9a353a1a6afe9059f2eb0e406bbecf
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-12 22:46:02 +0200

    Update BK_wallets.py

--- Metaverso_Crypto/inicio/Zona_depruebas/webadministrativa/Blockchain_Principal/Zona_de_pruebas/admin/pruebas2/Bk_Servidor/BK_Scr1/BK_dtbs/BK_wallets.py
+++ Metaverso_Crypto/inicio/Zona_depruebas/webadministrativa/Blockchain_Principal/Zona_de_pruebas/admin/pruebas2/Bk_Servidor/BK_Scr1/BK_dtbs/BK_wallets.py
@@ -1,1 +1,13 @@
+class Wallet:
+    def __init__(self):
+        self.private_key = self.generate_private_key()
+        self.public_key = self.generate_public_key()
 
+    def generate_private_key(self):
+        # Implementa la lógica de generación de clave privada aquí
+        pass
+
+    def generate_public_key(self):
+        # Implementa la lógica de generación de clave pública aquí
+        pass
+      
