476dd1128639a2a4b6acc595cced4f6c2591acf1
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-12 22:31:44 +0200

    Update BK_nerwork.py

--- Metaverso_Crypto/inicio/Zona_depruebas/webadministrativa/Blockchain_Principal/Zona_de_pruebas/admin/pruebas2/Bk_Servidor/BK_Scr1/BK_dtbs/BK_nerwork.py
+++ Metaverso_Crypto/inicio/Zona_depruebas/webadministrativa/Blockchain_Principal/Zona_de_pruebas/admin/pruebas2/Bk_Servidor/BK_Scr1/BK_dtbs/BK_nerwork.py
@@ -1,1 +1,11 @@
+class Network:
+    def __init__(self):
+        self.nodes = []
 
+    def add_node(self, node):
+        self.nodes.append(node)
+
+    def broadcast_transaction(self, transaction):
+        for node in self.nodes:
+            node.chain.add_transaction(transaction)
+          
