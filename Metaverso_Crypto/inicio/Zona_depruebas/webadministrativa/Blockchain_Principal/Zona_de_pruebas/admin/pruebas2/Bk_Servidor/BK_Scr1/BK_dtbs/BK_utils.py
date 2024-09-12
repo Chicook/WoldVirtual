6ed35e0f4f0a8e76a8b77efff275cf0250de2bf0
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-12 22:47:29 +0200

    Create BK_utils.py

---
+++ Metaverso_Crypto/inicio/Zona_depruebas/webadministrativa/Blockchain_Principal/Zona_de_pruebas/admin/pruebas2/Bk_Servidor/BK_Scr1/BK_dtbs/BK_utils.py
@@ -0,0 +1,7 @@
+def hash_data(data):
+    # Implementa la lógica de hash aquí
+    pass
+
+def validate_transaction(transaction):
+    # Implementa la lógica de validación de transacciones aquí
+    pass
