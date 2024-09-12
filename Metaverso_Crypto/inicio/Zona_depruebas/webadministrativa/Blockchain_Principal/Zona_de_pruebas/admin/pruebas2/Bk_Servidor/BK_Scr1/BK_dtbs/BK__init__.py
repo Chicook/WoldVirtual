4998df80b7b365e2cedbbd4650f428cac14cc978
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-12 22:54:49 +0200

    Update BK__init__.py

--- Metaverso_Crypto/inicio/Zona_depruebas/webadministrativa/Blockchain_Principal/Zona_de_pruebas/admin/pruebas2/Bk_Servidor/BK_Scr1/BK_dtbs/BK__init__.py
+++ Metaverso_Crypto/inicio/Zona_depruebas/webadministrativa/Blockchain_Principal/Zona_de_pruebas/admin/pruebas2/Bk_Servidor/BK_Scr1/BK_dtbs/BK__init__.py
@@ -1,13 +1,36 @@
-from ..BK_3D1
-from ..BK_Almacenamiento1
-from ..BK_App1
-from ..BK_dbmysql1
-from ..BK_bknts1
-from ..BK_main1
-from ..BK_svdc1
-from .Bk_bk1
-from ..BK_dbmysql1d 
-from ..BK_SBlokchain1
-from ..BK_svdc1
+# from ..BK_3D1
+# from ..BK_Almacenamiento1
+# from ..BK_App1
+# from ..BK_dbmysql1
+# from ..BK_bknts1
+# from ..BK_main1
+# from ..BK_svdc1
+# from .Bk_bk1
+# from ..BK_dbmysql1d 
+# from ..BK_SBlokchain1
+# from ..BK_svdc1
+from ..block import Block
+from ..chain import Blockchain
+from ..transaction import Transaction
+from ..node import Node
+from ..network import Network
+from ..consensus import Consensus
+from ..wallet import Wallet
+from ..utils import hash_data, validate_transaction
+from ..config import BLOCK_REWARD, DIFFICULTY
+from ..mining import Miner
 
-
+__all__ = [
+    "Block",
+    "Blockchain",
+    "Transaction",
+    "Node",
+    "Network",
+    "Consensus",
+    "Wallet",
+    "hash_data",
+    "validate_transaction",
+    "BLOCK_REWARD",
+    "DIFFICULTY",
+    "Miner"
+]
