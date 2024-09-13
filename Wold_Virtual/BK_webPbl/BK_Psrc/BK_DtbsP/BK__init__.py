e071fa5f5540c8d761ce538d44c7467de7298435
Jaldvox <166617485+Jaldvox@users.noreply.github.com>
2024-09-13 23:39:51 +0000

    add

--- Wold_Virtual/BK_webPbl/BK_Psrc/BK_DtbsP/BK__init__.py
+++ Wold_Virtual/BK_webPbl/BK_Psrc/BK_DtbsP/BK__init__.py
@@ -3,10 +3,21 @@ from .server import app as server_app
 from .reflex_app import app as reflex_app
 from .server import app as server_app
 from .reflex_app import MyApp
+import sys
+import os
 
+# Añadir el directorio BK_bkP1 al path
+sys.path.append(os.path.join(os.path.dirname(__file__), '../BK_blockchain_project/BK_bkP1/BK_main.py', 'BK_bkP1'))
+
+from main import app, blockchain, db
+
+if __name__ == '__main__':
+    # Añadir el bloque génesis a la base de datos interna
+        db.add_block(blockchain.chain[0])
+
+        
 def start_app():
     my_app = MyApp()
-        server_app.run(debug=True)
 
         if __name__ == '__main__':
             start_app()
