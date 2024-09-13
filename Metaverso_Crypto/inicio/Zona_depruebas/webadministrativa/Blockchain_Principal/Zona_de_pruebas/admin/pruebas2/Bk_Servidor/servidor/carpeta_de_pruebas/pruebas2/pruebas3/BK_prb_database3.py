c57491c16d22686e2fafa520493a6ae0c0254780
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-13 13:24:44 +0200

    Update and rename prb3.py to BK_prb_database3.py

---
+++ Metaverso_Crypto/inicio/Zona_depruebas/webadministrativa/Blockchain_Principal/Zona_de_pruebas/admin/pruebas2/Bk_Servidor/servidor/carpeta_de_pruebas/pruebas2/pruebas3/BK_prb_database3.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
+    conn = get_db_connection()
+    with open('schema.sql') as f:
+        conn.executescript(f.read())
+    conn.close()
