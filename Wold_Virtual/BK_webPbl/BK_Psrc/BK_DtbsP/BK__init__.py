d1ecfe62f299390599248cd9b6073891d6e264f8
Jaldvox <166617485+Jaldvox@users.noreply.github.com>
2024-09-14 01:43:35 +0200

    Update BK__init__.py

--- Wold_Virtual/BK_webPbl/BK_Psrc/BK_DtbsP/BK__init__.py
+++ Wold_Virtual/BK_webPbl/BK_Psrc/BK_DtbsP/BK__init__.py
@@ -1,26 +1,25 @@
-# BK__init__.py
-from .server import app as server_app
-from .reflex_app import app as reflex_app
-from .server import app as server_app
-from .reflex_app import MyApp
 import sys
 import os
 
+# Importar aplicaciones del servidor y Reflex
+from .server import app as server_app
+from .reflex_app import app as reflex_app
+from .reflex_app import MyApp
+
 # Añadir el directorio BK_bkP1 al path
-sys.path.append(os.path.join(os.path.dirname(__file__), '../BK_blockchain_project/BK_bkP1/BK_main.py', 'BK_bkP1'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'BK_blockchain_project', 'BK_bkP1'))
 
 from main import app, blockchain, db
 
-if __name__ == '__main__':
-    # Añadir el bloque génesis a la base de datos interna
-        db.add_block(blockchain.chain[0])
-
-        
+# Función para iniciar la aplicación
 def start_app():
     my_app = MyApp()
+    my_app.run()
 
-        if __name__ == '__main__':
-            start_app()
+if __name__ == '__main__':
+    # Añadir el bloque génesis a la base de datos interna
+    db.add_block(blockchain.chain[0])
+    # Ejecutar la API
+    app.run(debug=True)
 
 __all__ = ['server_app', 'reflex_app']
-
