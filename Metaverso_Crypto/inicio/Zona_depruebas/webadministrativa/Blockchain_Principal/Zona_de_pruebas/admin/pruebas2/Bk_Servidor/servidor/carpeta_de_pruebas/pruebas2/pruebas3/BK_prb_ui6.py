de58efb943b81d3c0bac210420e5fb92a86b5111
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-13 13:32:15 +0200

    Create BK_prb_ui6.py

---
+++ Metaverso_Crypto/inicio/Zona_depruebas/webadministrativa/Blockchain_Principal/Zona_de_pruebas/admin/pruebas2/Bk_Servidor/servidor/carpeta_de_pruebas/pruebas2/pruebas3/BK_prb_ui6.py
@@ -0,0 +1,11 @@
+from flask import render_template
+
+def create_ui(app):
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    @app.route('/login')
+    def login():
+        return render_template('login.html')
+      
