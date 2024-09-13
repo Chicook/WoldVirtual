5f317e79f3e85a1859316d8d1a729a381273cec9
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-13 13:27:28 +0200

    Update and rename prb4.py to BK_prb_render4.py

---
+++ Metaverso_Crypto/inicio/Zona_depruebas/webadministrativa/Blockchain_Principal/Zona_de_pruebas/admin/pruebas2/Bk_Servidor/servidor/carpeta_de_pruebas/pruebas2/pruebas3/BK_prb_render4.py
@@ -0,0 +1,24 @@
+# import pygame
+# from pygame.locals import *
+
+# def init_pygame():
+   # pygame.init()
+   # screen = pygame.display.set_mode((800, 600), DOUBLEBUF | OPENGL)
+    # glEnable(GL_DEPTH_TEST)
+    # glMatrixMode(GL_PROJECTION)
+   #  gluPerspective(45, (800/600), 0.1, 50.0)
+    # glTranslatef(0.0, 0.0, -5)
+    # return screen
+
+# def render_scene():
+    # screen = init_pygame()
+   #  running = True
+   #  while running:
+        # for event in pygame.event.get():
+            # if event.type == pygame.QUIT:
+               # running = False
+      #  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        # Aquí renderizarías tu modelo 3D
+       # pygame.display.flip()
+        # pygame.time.wait(10)
+   # pygame.quit()
