457ec2792ed17e97edee2f8e0d05b42e801b7c9d
Jaldvox <166617485+Jaldvox@users.noreply.github.com>
2024-09-13 17:46:01 +0000

    add

--- Wold_Virtual/BK_webPbl/BK_Psrc/BK_DtbsP/BK_RD/BK_Angular/BK_src/BK_App/BK_app.jsx
+++ Wold_Virtual/BK_webPbl/BK_Psrc/BK_DtbsP/BK_RD/BK_Angular/BK_src/BK_App/BK_app.jsx
@@ -5,7 +5,8 @@ const App = () => {
   return (
       <div>
             <h2>¡Hola desde React!</h2>
-                  <p>Este es un componente React integrado en una aplicación Angular.</p>
+                  <p>Este es un componente React 
+                    integrado en una aplicación Angular.</p>
                       </div>
                         );
                         };
