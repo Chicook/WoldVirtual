da8b3bdadc92c15139a652b271bb1d252142448f
Jaldvox <166617485+Jaldvox@users.noreply.github.com>
2024-09-14 18:17:24 +0000

    add

---
+++ Wold_Virtual/BK_Adm_prb/BK_Recursos/BK_Unity/inicio.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unityengine;
+
+public class BK_primerscryp : MonoBehviour
+
+
+    void Start();
+    {
+        Debug.log("wold virtual");
+    }
+
+
+    
+}
+
+//
+{
+//    public global::System.Object Item1;
+    //public global::System.Object Item2;
+
+    //public NewStruct(global::System.Object item1, global::System.Object item2)
+   // {
+        //Item1 = item1;
+        //Item2 = item2;
+   // }
+
+    //public override global::System.Boolean Equals(global::System.Object obj)
+   / {
+    /   // return obj is NewStruct other &&
+             //  EqualityComparer<global::System.Object>.Default.Equals(Item1, other.Item1) &&
+              // EqualityComparer<global::System.Object>.Default.Equals(Item2, other.Item2);
+    }
+
+    //public override global::System.Int32 GetHashCode()
+   // {
+      //  global::System.Int32 hashCode = -1030903623;
+       // hashCode = hashCode * -1521134295 + EqualityComparer<global::System.Object>.Default.GetHashCode(Item1);
+       // hashCode = hashCode * -1521134295 + EqualityComparer<global::System.Object>.Default.GetHashCode(Item2);
+       // return hashCode;
+  //  }
+
+    
+}
