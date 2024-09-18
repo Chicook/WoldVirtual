6a53a3f49e91ab55aa18f757f24ed696199bac9e
Jaldvox <166617485+Jaldvox@users.noreply.github.com>
2024-09-18 15:28:05 +0000

    add

---
+++ Web_administracion/BK_INICIO/secciones/sección_1/csharp/Program.cs
@@ -0,0 +1,11 @@
+// BK_INICIO/seccion_1/csharp/Program.cs
+
+using System;
+
+class Program
+{
+    static void Main()
+        {
+    Console.WriteLine("¡Bienvenido a tu web básica en C#!");
+    }
+}
---
+++ Web_administracion/BK_INICIO/secciones/sección_1/react/App.jsx
@@ -0,0 +1,15 @@
+// BK_INICIO/seccion_1/react/App.jsx
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+      <div>
+            <h1>¡Bienvenido a tu web básica en React!</h1>
+                </div>
+                  );
+                  }
+
+                  ReactDOM.render(<App />, document.getElementById('root'));
+                  
