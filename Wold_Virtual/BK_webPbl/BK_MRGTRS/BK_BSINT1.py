a179c9440df4a5cdbebad76ec059b0878aa02c76
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-09-23 10:11:02 +0000

    add

--- Wold_Virtual/BK_webPbl/BK_MRGTRS/BK_BSINT1.py
+++ Wold_Virtual/BK_webPbl/BK_MRGTRS/BK_BSINT1.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_socketio import SocketIO
-from BK_BSINT2 import register_routes
-from BK_BSINT3 import handle_register, handle_delete_user
+from Wold_Virtual.BK_webPbl.BK_MRGTRS.BK_BSINT2 import register_routes
+from Wold_Virtual.BK_webPbl.BK_MRGTRS.BK_BSINT3 import handle_register, handle_delete_user
 
 app = Flask(__name__)
 socketio = SocketIO(app)
