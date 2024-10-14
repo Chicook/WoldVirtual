43b9a3b0e9ce3b3accd29e33e1385e89844f790b
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-10-14 22:43:23 +0200

    Update BK_App.py

--- Web_administracion/BK_BKCHM/BK_App.py
+++ Web_administracion/BK_BKCHM/BK_App.py
@@ -5,12 +5,12 @@ from BK_TRSC import BK_TRSC
 
 def main():
     # Crear blockchain
-        blockchain = Blockchain()
+        blockchain = BK_BCHPM()
 
             # Crear algunas transacciones
-                t1 = Transaction("Alice", "Bob", 10)
-                    t2 = Transaction("Bob", "Charlie", 5)
-                        t3 = Transaction("Charlie", "Dave", 7)
+                t1 = BK_TRSC("Alice", "Bob", 10)
+                    t2 = BK_TRSC("Bob", "Charlie", 5)
+                        t3 = BK_TRSC("Charlie", "Dave", 7)
 
                             # Añadir bloques con transacciones
                                 blockchain.add_block([t1, t2])
