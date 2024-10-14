03175915d42608c749767b32ba7339a71fc7b71d
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-10-14 19:59:03 +0000

    add

---
+++ Web_administracion/BK_BKCHM/BK_App.py
@@ -0,0 +1,29 @@
+# app.py
+
+from blockchain import Blockchain
+from transactions import Transaction
+
+def main():
+    # Crear blockchain
+        blockchain = Blockchain()
+
+            # Crear algunas transacciones
+                t1 = Transaction("Alice", "Bob", 10)
+                    t2 = Transaction("Bob", "Charlie", 5)
+                        t3 = Transaction("Charlie", "Dave", 7)
+
+                            # Añadir bloques con transacciones
+                                blockchain.add_block([t1, t2])
+                                    blockchain.add_block([t3])
+
+                                        # Imprimir la blockchain
+                                            print("Blockchain:")
+                                                for block in blockchain.chain:
+                                                        print(block)
+
+                                                            # Verificar la validez de la cadena
+                                                                print("¿Es válida la cadena?", blockchain.is_chain_valid())
+
+                                                                if __name__ == "__main__":
+                                                                    main()
+                                                                    
